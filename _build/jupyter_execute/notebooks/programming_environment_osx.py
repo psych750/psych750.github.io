401d72d23c79c3e0bf0b55dc0bb72c2fddb7bfef
Gary Lupyan <glupyan@gmail.com>
2022-08-16 13:28:45 -0500

    Modified lists; added functions

--- _build/jupyter_execute/notebooks/programming_environment_osx.py
+++ _build/jupyter_execute/notebooks/programming_environment_osx.py
@@ -5,7 +5,7 @@
 # 
 # Python is installed by default on Macs, but we will ***not*** be using the standard install. Instead we will be using the Anaconda environment which allows for easy installation of packages into a virtual environment which helps to eliminate software conflicts. 
 
-# # Install git
+# ## Install git
 # [Download and install](https://sourceforge.net/projects/git-osx-installer/files/) the latest version of `git` 
 # 
 # Test it by typing 
@@ -13,17 +13,18 @@
 # at the command prompt.
 # 
 # 
-# # Install the Anaconda Distribution of Python
+
+# ## Install the Anaconda Distribution of Python
 # 
 # ```{attention}
 # The instructions are slightly different if you are using an Intel Mac or a newer M1/M2 mac. If you're not sure Click on the Apple icon on the top left corner and select `About this Mac`. If it says Intel XXX, then follow the Intel CPU instructions. If it says Apple M1 or similar, follow the Mac CPU instructions.
 # ```
 # 
-# ## Intel CPU Instructions
+# ### Intel CPU Instructions
 # * Download and Install the **Python 3.8 64 bit** [Miniconda installer](https://conda.io/miniconda.html). If you are asked whether you want the installer to prepend Anaconda to your PATH, respond ***yes***.
 # 
 # 
-# ## Mac CPU Instructions
+# ### Mac CPU Instructions
 # * Open up your `Finder` and click on `Applications`. Then navigate to `Utilities` folder and right click on the `Terminal` and select Duplicate. Rename the duplicate to `Rosetta-Terminal`. Then right click on the `Rosetta-Terminal` and select `Get Info`. Check the `Open with Rosetta` option.
 # 
 # * Download and Install the **Python 3.8 64 bit** [Miniconda installer](https://conda.io/miniconda.html). Make sure to select the `macOS Intel x86 64-bit` version. The `pkg` file is more user friendly and installs like any other programs. If you are asked whether you want the installer to prepend Anaconda to your PATH, respond ***yes***.
@@ -31,7 +32,10 @@
 # * From now on, use the `Rosetta-Terminal` instead of the original `Terminal`.
 # 
 # 
-# # Create Virtual Environment
+
+# 
+# 
+# ## Create a Virtual Environment
 # 
 # Next let's create a virtual environment for this course. At the command prompt type
 # 
@@ -44,7 +48,7 @@
 # conda activate psych750
 # ```
 # 
-# ## Test the Anaconda Python install
+# ### Test the Anaconda Python install
 # 
 # Activate your environment using `conda activate psych750` and then type `python` to start the Python interpreter
 # ```bash
@@ -54,7 +58,9 @@
 # Type "help", "copyright", "credits" or "license" for more information.
 # ```
 # 
-# ## Install Homebrew
+# 
+
+# ## Install Homebrew and PortAudio
 # 
 # Next we need to install Homebrew and run some commands in order to install other packages. Follow the instructions [here](https://brew.sh) to install Homebrew.
 # 
@@ -62,9 +68,11 @@
 # ```bash
 # brew install portaudio
 # ```
+# This will install some libraries for doing some speech recognition magic
+
 # 
 # ## Installing the necessary Python packages
-# * Now let's install the packages we'll need. Paste in the lines below, one at at a time, into your terminal (make sure your new environment is activated first). Wait for everything to install before proceeding to the next line.
+# Now let's install the packages we'll need. Paste in the lines below, one at at a time, into your terminal (make sure your new environment is activated first). Wait for everything to install before proceeding to the next line.
 # 
 # ```bash
 # conda install jupyter jupyterlab
@@ -76,7 +84,7 @@
 # If you run into any install errors, please make note of the precise errors and post them to the #installation_problems Slack channel.
 # ```
 # 
-# ## Test PsychoPy
+# ### Test PsychoPy
 # ====
 # * Now let's test that `PsychoPy` installed correctly. Start the python interpreter and then import the psychopy package:
 # 
@@ -124,7 +132,11 @@
 # 
 # If you saw a little pop-up window, all's good. If it doesn't immediately appear, check that it's not behind your active window.
 # 
-# ## Configure a few Psychopy settings
+# 
+# 
+
+# 
+# ### Configure a few Psychopy settings
 # 
 # While we're here, let's also make sure the Psychopy settings are in order. Enter these lines in the python interpreter:
 # ```python
@@ -133,24 +145,53 @@
 # psychopy.prefs.hardware['audioLib'] = ['PTB', 'pyo','pygame']
 # psychopy.prefs.saveUserPrefs()
 # ```
-# 
+
 # ## Install Visual Studio Code
 # 
 # Visual Studio Code is a very powerful and user-friendly code editor which we will use in this class to edit python/jupyter notebook scripts. Download Visual Studio Code [here](https://code.visualstudio.com) and install it.
 # 
 # Once it's installed, open up VS code and select File -> New Text File. At the top of the script, click select a language and select `python`. VS code will prompt you to install the Python extension. Once that is installed go back to your file and press `Command + Shift + P`, and type in `Python: Select Interpreter`. Select the `psych750` conda Python environment you created earlier. 
 # 
+# Look over keyboard shortcuts: `Help: Keyboard Shortcut Reference` Try to use them!
+# 
 # ### Create a shortcut to Visual Studio code
 # 
-# It is very useful to be able to quickly access your editor from the shell.
+# It is very useful to be able to quickly open up a file in the editor from the shell. You can do this like so:
 # 
-# ...
+# Hit  `Command + Shift + P` and type `shell` 
+# Press `enter` to `Install Code Command in PATH`
+# 
+# 
+# Test the installation by launching a new terminal window and typing
+# ```bash
+# code some_file_name.txt
+# ```
+# 
+# where `some_file_name.txt` is the file you want to open. 
+# 
+# It should open up in visual Studio Code
+# 
+# If you want to shorten it further, e.g., instead of having to type `code` you want to type `c`, add an `alias` to your shell configuration file (`.bash_profil), like so:
+# 
+# ```bash
+# code ~/.bash_profile
+# ```
+# 
+# Now add the line
+# ```bash
+# alias c="code"
+# ```
+# 
+# Save the file and restart your shell. You should now be able to open files in Visual Code Studio by typing
+# ```bash
+# c some_file_name.txt
+# ```
 
-# # Install R & RStudio Desktop
+# ## Install R & RStudio Desktop
 # 
 # Next install R and RSudio Desktop by following the instructions [here](https://www.rstudio.com/products/rstudio/download/#download).
 # 
-# ## Install a few R packages
+# ### Install a few R packages
 # 
 # Once R and RStudio are installed, make sure you can open up RStudio successfully and then install these packages (we may include some additional ones later).
 # 
